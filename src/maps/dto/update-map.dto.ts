bf3372c3a75487c85785be37e57d2f944696ba3f
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-08-27 05:25:32 +0900

    사용하지 않는 요소 제거

--- src/maps/dto/update-map.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMapDto } from './create-map.dto';
-
-export class UpdateMapDto extends PartialType(CreateMapDto) {}
--- src/maps/maps.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MapsService } from './maps.service';
-import { MapsController } from './maps.controller';
-
-@Module({
-  controllers: [MapsController],
-  providers: [MapsService]
-})
-export class MapsModule {}
