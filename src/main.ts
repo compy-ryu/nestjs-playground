7b097648c75222d77ded101f614e5ab901ea413b
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-08-27 02:24:14 +0900

    swagger 설치

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('콤피의 php 탈출기')
+    .setDescription('콤피는 NestJS를 쓰고 싶어요')
+    .setVersion('0.0.1')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
