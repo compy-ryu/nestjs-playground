07ec6a91bdd1eea8fd5919d4d1638e857466cefe
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-08-27 07:34:10 +0900

    환경변수 세팅

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
+import { DatabaseConfig } from './config/db.config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService,
+  ) {}
 
   @Get()
-  getHello(): string {
+  getHelloWorld(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/dbhost')
+  getDatabaseHost(): string {
+    // env 테스트용
+    const dbConfig = this.configService.get<DatabaseConfig>('database');
+    return dbConfig.host;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompyModule } from './compy/compy.module';
-import { MapsModule } from './maps/maps.module';
+import { UserModule } from './user/user.module';
+
+import databaseConfig from './config/db.config';
 
 @Module({
-  imports: [CompyModule, MapsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    CompyModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
